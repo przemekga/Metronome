3f3a77560c2eb145d35f7272b7e4f3e98a1d00f0
pemol117 <pemol10@gmail.com>
2016-04-28 00:36:53 +0200

    dynamic tempo change added

--- metronome.js
+++ metronome.js
@@ -1,10 +1,30 @@
-$(function(){
-    var bpm = 120;
-    //var speedMs = 60000 / bpm;
-    var interval;
-    var start = false;
+var start = false;
+var bpm = 60;
+var speedMs = 60000;
+var interval;
+var st = $('.start');
+$(function(){    
+    $('.start').on('click', function(){
+        
+        if (st.text() === 'START') {
+            st.text('STOP');
+        }
+        else {
+            st.text('START');
+        }
+        buttonPress(bpm, start);
+    });    
     
     
+    $('.preset').on('click', function() {
+        var sp = $(this).text();
+        st.text('STOP');
+        clearInterval(interval);
+        buttonPress(sp, false);
+        
+        $('#speed').text(sp);
+    });   
+
     function click() {
         if (!start) {
             clearInterval(interval);
@@ -13,24 +33,17 @@ $(function(){
         audio = new Audio();
         audio.src = "audio/click.mp3";
         audio.play();
-    };
+    };    
     
-    $('.start').on('click', function(){
-        if (!start) {
-            start = true;
-            interval = setInterval(click, 60000 / bpm);
+    function buttonPress(bpmX, startX) {
+        if (!startX) {
+            startX = true;
+            interval = setInterval(click, speedMs / bpmX);
         }
         else {
-            start = false;
-            click();
+            startX = false;
         };
-    });    
-    
-    
-    $('.preset').on('click', function() {
-        var sp = $(this).text();
-        bpm = sp;
-        $('#speed').text(sp);    
-    });   
-    
+        start = startX;
+    }
 });
+
