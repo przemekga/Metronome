829fd722fab57f4221b223b612a8c3914f304f1f
pemol117 <pemol10@gmail.com>
2016-09-22 18:07:20 +0200

    config

--- metronome.js
+++ metronome.js
@@ -1,10 +1,4 @@
-// var start = false;      //default state - off
-// var speedMs = 60000;    //milliseconds in one second
-// var interval;           //Interval id for clearInterval
-// var st = $('.start');   //START button
-// var presetSpeed = 60;   //preset bpm (60 as default)
-
-$(function(){
+var metronome = function(){
     var presetSpeed = 60;
     var config = {
         settings: {
@@ -21,80 +15,89 @@ $(function(){
             preset: $('.preset')
         }
     }
-    //START button
-    
-    config.selectors.st.on('click', function(){
-        
-        if (config.selectors.st.text() === 'START') {
-            config.selectors.st.text('STOP');
-        }
-        else {
-            config.selectors.st.text('START');
+
+    function init() {
+
+        //START button
+        config.selectors.st.on('click', function () {
+
+            if (config.selectors.st.text() === 'START') {
+                config.selectors.st.text('STOP');
+            }
+            else {
+                config.selectors.st.text('START');
+                clearInterval(config.settings.interval);
+            }
+            buttonPress(presetSpeed, config.settings.start);
+        });
+
+        //Plus and minus
+
+        config.selectors.plus.on('click', function () {
             clearInterval(config.settings.interval);
-        }
-        buttonPress(presetSpeed, config.settings.start);
-    });    
-    
-    //Plus and minus
+            presetSpeed++;
+            config.selectors.speed.text(presetSpeed);
+            if (config.settings.start) {
+                buttonPress(presetSpeed, false);
+            }
+        });
 
-    config.selectors.plus.on('click', function() {
-        clearInterval(config.settings.interval);
-        presetSpeed++;
-        config.selectors.speed.text(presetSpeed);
-        if (start) {
-            buttonPress(presetSpeed, false);
-        }
-    });
-
-    config.selectors.minus.on('click', function() {
-        clearInterval(config.settings.interval);
-        if (presetSpeed > 0) {
-            presetSpeed--;
-        }
-        else {
-            return;
-        }
-        config.selectors.speed.text(presetSpeed);
-        if (start) {
-            buttonPress(presetSpeed, false);
-        }
-    });
-    
-    
-    //Preset buttons
-
-    config.selectors.preset.on('click', function() {
-        presetSpeed = $(this).text();
-        clearInterval(config.settings.interval);
-        if (start) {
-            buttonPress(presetSpeed, false);
-        }
-        config.selectors.speed.text(presetSpeed);
-    });   
-    
-    //Metronome sound function
-    
-    function click() {
-        if (!start) {
+        config.selectors.minus.on('click', function () {
             clearInterval(config.settings.interval);
-            return;
-        }
-        audio = new Audio();
-        audio.src = "audio/click.mp3";
-        audio.play();
-    };    
-    
-    //Function for switching on/off
-    
-    function buttonPress(bpmX, startX) {
-        if (!startX) {
-            startX = true;
-            config.settings.interval = setInterval(click, speedMs / bpmX);
-        }
-        else {
-            startX = false;
+            if (presetSpeed > 0) {
+                presetSpeed--;
+            }
+            else {
+                return;
+            }
+            config.selectors.speed.text(presetSpeed);
+            if (config.settings.start) {
+                buttonPress(presetSpeed, false);
+            }
+        });
+
+
+        //Preset buttons
+
+        config.selectors.preset.on('click', function () {
+            presetSpeed = $(this).text();
+            clearInterval(config.settings.interval);
+            if (config.settings.start) {
+                buttonPress(presetSpeed, false);
+            }
+            config.selectors.speed.text(presetSpeed);
+        });
+
+        //Metronome sound function
+
+        function click() {
+            if (!config.settings.start) {
+                clearInterval(config.settings.interval);
+                return;
+            }
+            audio = new Audio();
+            audio.src = "audio/click.mp3";
+            audio.play();
         };
-        start = startX;
+
+        //Function for switching on/off
+
+        function buttonPress(bpmX, startX) {
+            if (!startX) {
+                startX = true;
+                config.settings.interval = setInterval(click, config.settings.speedMs / bpmX);
+            }
+            else {
+                startX = false;
+            }
+
+            config.settings.start = startX;
+        }
     }
-});
+    return {
+        init:init,
+        config:config
+    };
+}();
+metronome.init();
 
